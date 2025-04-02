264b574543b0d3d3eaa91cba6d30ecb358cb3bee
Skyler Burden <burdensk@mail.gvsu.edu>
2025-04-02 09:41:57 -0400

    added login button to homescreen

--- review_application/src/Home.js
+++ review_application/src/Home.js
@@ -1,7 +1,14 @@
 // src/Home.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate(); // initialize useNavigate
+
+  const handleLoginClick = () => {
+    navigate('/login'); // navigate to the /login page
+  };
+
   return (
     <div className="home">
       <div className='home-screen-icons-container'>
@@ -13,6 +20,7 @@ function Home() {
         <h2>Your go-to place for logging reviews for books and games!</h2>
         <h2>Explore our collection of reviews or make an account to make your own!</h2>
       </div>
+      <button onClick={handleLoginClick} className="login-button">Go to Login</button>
     </div>
   );
 }
