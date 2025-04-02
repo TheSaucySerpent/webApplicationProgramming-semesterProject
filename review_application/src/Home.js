904436bdd2a94ffbce52497ff74c07582d269c5b
Skyler Burden <burdensk@mail.gvsu.edu>
2025-04-02 09:38:00 -0400

    added some basic styling to home screen

--- review_application/src/Home.js
+++ review_application/src/Home.js
@@ -4,9 +4,15 @@ import React from 'react';
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to allMyReviews!</h1>
-      <p>Your go-to place for logging reviews for books and games!</p>
-      <p>Explore our collection of reviews or make an account to make your own</p>
+      <div className='home-screen-icons-container'>
+        <img src='/images/walking_book.png' className='home-screen-icon' id='book-icon' alt='Walking Book'></img>
+        <img src='/images/arcade_cabinet.png' className='home-screen-icon'  id='arcade-cabinet-icon'alt='Arcade Cabinet'></img>
+      </div>
+      <div className="home-screen-text-container">
+        <h1>Welcome to allMyReviews!</h1>
+        <h2>Your go-to place for logging reviews for books and games!</h2>
+        <h2>Explore our collection of reviews or make an account to make your own!</h2>
+      </div>
     </div>
   );
 }
