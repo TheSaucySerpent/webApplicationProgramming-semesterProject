3a971cb591314b4280fd8bdb7a0f62d4fb8385f8
Skyler Burden <burdensk@mail.gvsu.edu>
2025-04-01 23:03:01 -0400

    aligned home page text in center

--- review_application/src/Home.js
+++ review_application/src/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to allMyReviews</h1>
+      <h1>Welcome to allMyReviews!</h1>
       <p>Your go-to place for logging reviews for books and games!</p>
       <p>Explore our collection of reviews or make an account to make your own</p>
     </div>
