be47ad71d3ca64df2fbcc870da0c05f8ac6f211b
Skyler Burden <burdensk@mail.gvsu.edu>
2025-04-02 21:26:24 -0400

    reworked home page to use react bootstrap and added a carousel

--- review_application/src/Home.js
+++ review_application/src/Home.js
@@ -1,6 +1,7 @@
 // src/Home.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Container, Row, Col, Button, Carousel } from 'react-bootstrap';
 
 function Home() {
   const navigate = useNavigate(); // initialize useNavigate
@@ -10,19 +11,37 @@ function Home() {
   };
 
   return (
-    <div className="home">
-      <div className='home-screen-icons-container'>
-        <img src='/images/walking_book.png' className='home-screen-icon' id='book-icon' alt='Walking Book'></img>
-        <img src='/images/arcade_cabinet.png' className='home-screen-icon'  id='arcade-cabinet-icon'alt='Arcade Cabinet'></img>
-      </div>
-      <div className="home-screen-text-container">
-        <h1>Welcome to allMyReviews!</h1>
-        <h2>Your go-to place for logging reviews for books and games!</h2>
-        <h2>Explore our collection of reviews or make an account to make your own!</h2>
-      </div>
-      <button onClick={handleLoginClick} className="login-button">Go to Login</button>
-    </div>
-  );
+    <Container fluid className='vh-100 d-flex flex-column justify-content-center align-items-center'>
+      <Row className='mb-4'>
+        <Carousel className='custom-carousel'>
+          <Carousel.Item interval={5000}>
+            <img src='/images/carousel_bookshelf.jpg' alt='Carousel Bookshelf'></img>
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <img src='/images/carousel_arcade.jpg' alt='Carousel Gameroom'></img>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+      <Row>
+        <Col className='d-flex justify-content-center'>
+          <img src='/images/walking_book.png' className='home-screen-icon' id='book-icon' alt='Walking Book' />
+          <img src='/images/arcade_cabinet.png' className='home-screen-icon' id='arcade-cabinet-icon' alt='Arcade Cabinet'/>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='text-center'>
+          <h1>Welcome to reviewSpot!</h1>
+          <h2>Your go-to place for logging reviews for books and games!</h2>
+          <h2>Explore our collection of reviews or make an account to make your own!</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button onClick={handleLoginClick} variant='primary'>Go to Login</Button>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Home;
--- review_application/src/Login.js
+++ review_application/src/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { authentication } from './firebaseConfig';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const Login = (props) => {
   const [displayName, setDisplayName] = useState('');
@@ -8,26 +9,27 @@ const Login = (props) => {
   const [password, setPassword] = useState('');
   const [isRegistering, setIsRegistering] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
 
   async function handleSubmit(event) {
-    event.preventDefault(); // prevent default form submission behavior
+    event.preventDefault(); // prevent default form submission behavior (page reload)
     try {
       if (isRegistering) {
         // create a new user with the email and password
         const userCredential = await createUserWithEmailAndPassword(authentication, email, password);
-
         // update the user's display name
         await updateProfile(userCredential.user, {
           displayName: displayName
         });
-      }
+      } 
       else {
         // sign in the established user
         await signInWithEmailAndPassword(authentication, email, password);
       }
-      props.onLogin()
-    }
-    catch(error) {
+      props.onLogin(); // call onLogin prop
+      navigate('/book-reviews'); // navigate to /book-reviews after login
+    } 
+    catch (error) {
       // handle Firebase authentication errors
       if (error.code === 'auth/email-already-in-use') {
         setErrorMessage('This email is already in use. Please log in instead.');
@@ -46,13 +48,14 @@ const Login = (props) => {
       <h1>{isRegistering ? 'Create Account' : 'Login'}</h1>
       {errorMessage ? <p>{errorMessage}</p> : null}
       <form onSubmit={handleSubmit} className='login-form'>
-        {isRegistering && (<input
-          type="display-name"
-          placeholder="Display Name"
-          value={displayName}
-          onChange={(event) => setDisplayName(event.target.value)}
-          required>
-          </input>
+        {isRegistering && (
+          <input
+            type="text"
+            placeholder="Display Name"
+            value={displayName}
+            onChange={(event) => setDisplayName(event.target.value)}
+            required
+          />
         )}
         <input
           type="email"
@@ -71,15 +74,16 @@ const Login = (props) => {
         <button type="submit">{isRegistering ? 'Register' : 'Login'}</button>
       </form>
       <div className='toolbar'>
-        <button type="toggle-is-registering" onClick={() => {
+        <button type="button" onClick={() => {
           setIsRegistering(!isRegistering);
-          setErrorMessage(''); // clear error message when switching modes
+          setErrorMessage(''); // Clear error message when switching modes
         }}>
           {isRegistering ? 'I already have an account' : 'Register'}
         </button>
-        <button type="continue-without-login" onClick={() => {
-          props.onLogin(); // act like the user has just logged in
-          setErrorMessage(''); // clear error message when bypassing login
+        <button type="button" onClick={() => {
+          props.onLogin(); // Act like the user has just logged in
+          setErrorMessage(''); // Clear error message when bypassing login
+          navigate('/book-reviews'); // Navigate to /book-reviews
         }}>
           Continue Without Login
         </button>
