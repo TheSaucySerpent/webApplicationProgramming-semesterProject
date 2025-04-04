97c3db86ccfff9270f5e8c49d02458737f1ff2c0
Skyler Burden <burdensk@mail.gvsu.edu>
2025-04-03 23:52:09 -0400

    started using bootstrap for reviews

--- review_application/src/components/Review.js
+++ review_application/src/components/Review.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col, Button} from 'react-bootstrap';
 
 function Review(props) {
   const getReviewStars = (rating) => {
@@ -7,21 +8,54 @@ function Review(props) {
     return "⭐".repeat(fullStars) + (halfStars ? "½" : "");
   }
 
-  return <div className="review">
-    <div className="review-header">
-      <h2>{props.title} - {props.author}</h2>
-      <p>Reviewed by: {props.displayName}</p>
-      <div className="button-container">
-        <button id='edit-button' onClick={() => props.onEdit(props)}>Edit</button>
-        <button id='delete-button' onClick={() => props.onDelete(props.isbn)}>Delete</button>
-      </div>
-    </div>
+  return (
+    <Container className='review'>
+      <Row>
+        <Col>
+          <h2>{props.title} - {props.author}</h2>
+        </Col>
+        <Col>
+          <p>Reviewed by: {props.displayName}</p>
+        </Col>
+      </Row>
+      <Row className='d-flex justify-content-end'>
+        <Col xs="auto mb-3">
+          <Button variant="warning" onClick={() => props.onEdit(props)}>Edit</Button>
+        </Col>
+        
+        <Col xs="auto mb-3">
+          <Button variant="danger" onClick={() => props.onDelete(props.isbn)}>Delete</Button>
+        </Col>
+      </Row>
+      <Row>
+        <p>ISBN: {props.isbn}</p>
+      </Row>
+      <Row>
+        <p>Ranking: {getReviewStars(props.ranking)}</p>
+      </Row>
+      <Row>
+        <p>Release Year: {Math.abs(props.release_year)} {props.release_year < 0 ? "BC" : ""}</p>
+      </Row>
+      <Row>
+        <p>Review: {props.review}</p>
+      </Row>
+    </Container>
+  );
+  // return <div className="review">
+  //   <div className="review-header">
+  //     <h2>{props.title} - {props.author}</h2>
+  //     <p>Reviewed by: {props.displayName}</p>
+  //     <div className="button-container">
+  //       <button id='edit-button' onClick={() => props.onEdit(props)}>Edit</button>
+  //       <button id='delete-button' onClick={() => props.onDelete(props.isbn)}>Delete</button>
+  //     </div>
+  //   </div>
 
-    <p>ISBN: {props.isbn}</p>
-    <p>Ranking: {getReviewStars(props.ranking)}</p>
-    <p>Release Year: {Math.abs(props.release_year)} {props.release_year < 0 ? "BC" : ""}</p>
-    <p>Review: {props.review}</p>
-  </div>
+  //   <p>ISBN: {props.isbn}</p>
+  //   <p>Ranking: {getReviewStars(props.ranking)}</p>
+  //   <p>Release Year: {Math.abs(props.release_year)} {props.release_year < 0 ? "BC" : ""}</p>
+  //   <p>Review: {props.review}</p>
+  // </div>
 }
 
 export default Review;
