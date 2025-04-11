719ed8c230eaa01acfde85d3564b7837e98ede99
Skyler Burden <burdensk@mail.gvsu.edu>
2025-04-11 00:27:52 -0400

    started using environment variables for deployment

---
+++ review_application/src/firebaseConfig.js
@@ -0,0 +1,23 @@
+// src/firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebasestorage.com`,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app); // initialize Firestore
+const authentication = getAuth(app); // initialize Authentication
+// const analytics = getAnalytics(app); // initialize analytics
+
+export { firestore, authentication};
